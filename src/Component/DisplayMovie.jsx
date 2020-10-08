33f4833c79a3749d6da76fdb1d8a9ab9dde719f6
Monika98-mishra <monikamishra7198@gmail.com>
2020-10-08 17:13:36 +0530

    code_wave assignment

--- src/Component/DisplayMovie.jsx
+++ src/Component/DisplayMovie.jsx
@@ -1,35 +1,31 @@
 import React, { useState } from 'react';
 import { useMemo } from 'react';
 import './DisplayMovie.css';
-import {BrowserRouter as Router, Route , Switch  , Link} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-function DisplayCity({moviedata, setResult}) {
-    
+function DisplayCity({ moviedata, setResult, fetchMore, currentPage, totalpages }) {
+
+    console.log(moviedata, currentPage, totalpages);
 
     const [search, setSearch] = useState("");
-    
 
-    
 
     const renderlist = (moviedata) => {
         if (moviedata.length > 0) {
-            
-            return moviedata.map((item, index) => {
-
-                return (
-                    <div className="cards">
+            return (
+                moviedata.map((item, index) => {
+                    return <div className="cards">
                         <div key={index} className="card">
-                            { console.log(item) }
+                            {console.log(item)}
                             <img src={item.Poster} width="150" alt={item.Title} />
                             <h4><a href="#" ><Link to={`/DetailsMovie/${item.imdbID}`}>{item.Title}</Link></a></h4>
                         </div>
-                        
+
                     </div>
+                })
+            )
 
-
-                )
-
-            })
         } else {
             return <p>No data found</p>
         }
@@ -37,18 +33,26 @@ function DisplayCity({moviedata, setResult}) {
 
     const changefn = (e) => {
         setSearch(e.target.value);
-        if(e.target.value) {
-            let filteredData = moviedata.filter(a=> a.Title.toLowerCase().includes(search));
+        if (e.target.value) {
+            let filteredData = moviedata.filter(a => a.Title.toLowerCase().includes(search));
             filteredData.length > 0 ? setResult(filteredData, true) : setResult(filteredData, false);
         } else {
             setResult([], true);
         }
-            
+
     }
+
+    // fetchMore
 
 
     return (
         <div >
+            <InfiniteScroll
+                dataLength={moviedata.length}
+                next={fetchMore}
+                hasMore={totalpages - currentPage !== 0}
+                loader={<h4>Loading...</h4>}
+            ></InfiniteScroll>
             <center>
                 {/* <h2>Search Movie</h2> */}
                 <table className="titlebar">
@@ -67,12 +71,6 @@ function DisplayCity({moviedata, setResult}) {
             <div className="cards">
                 {renderlist(moviedata)}
             </div>
-
-
-
-
-
-
         </div>
     )
 }
--- src/Component/HooksComponent.jsx
+++ src/Component/HooksComponent.jsx
@@ -3,23 +3,27 @@ import DisplayMovie from './DisplayMovie.jsx';
 
 
 
-const url = `http://www.omdbapi.com/?apikey=230afcff&s=Marvel&type=movie`;
+
 
 function Hooks() {
     const [movie, setmovie] = useState([]);
+    const [page, setpage] = useState(1);
     const [allmovie, setAllMovie] = useState([]);
+    const [totalpages, setTotalpages] = useState(0);
 
 
     useEffect(() => {
-        getResult()
+        getResult(page)
     }, []);
 
-    const getResult = async () => {
+    const getResult = async (page) => {
+        const url = `http://www.omdbapi.com/?apikey=230afcff&s=Marvel&type=movie&page=${page}`;
         await fetch(url)
             .then((res) => res.json())
             .then((data) => {
-                setAllMovie(data.Search);
-                setmovie(data.Search)
+                setAllMovie([...allmovie ,data.Search]);
+                setmovie([...movie, data.Search]);
+                setTotalpages(Math.floor(Number(data.totalResults)/10));
             });
     }
 
@@ -36,12 +40,18 @@ function Hooks() {
 
     }
 
-
+    const fetchMore = () => {
+        let _newpage = page+1;
+            setpage(_newpage);
+            getResult(_newpage);
+   
+    }
 
 
     return (
         <div>
-            <DisplayMovie moviedata={movie} setResult={setResult} />
+            <DisplayMovie moviedata={movie} setResult={setResult} fetchMore={fetchMore} totalpages={totalpages}
+            currentPage={page} />
         </div>
     )
 }
