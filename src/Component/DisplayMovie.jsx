833639a6b511014e8743e7f4ffa4315ae4ebe3fa
Monika98-mishra <monikamishra7198@gmail.com>
2020-10-08 21:21:24 +0530

    codewave_test

--- src/Component/DisplayMovie.jsx
+++ src/Component/DisplayMovie.jsx
@@ -1,29 +1,33 @@
-import React, { useState } from 'react';
-import { useMemo } from 'react';
+import React from 'react';
 import './DisplayMovie.css';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-function DisplayCity({ moviedata, setResult, fetchMore, currentPage, totalpages }) {
-
-    console.log(moviedata, currentPage, totalpages);
-
-    const [search, setSearch] = useState("");
-
+function DisplayCity({ moviedata, fetchMore, currentPage, totalpages, search, updateSearch }) {
 
     const renderlist = (moviedata) => {
         if (moviedata.length > 0) {
+            console.log(totalpages - currentPage !== 0);
             return (
-                moviedata.map((item, index) => {
-                    return <div className="cards">
-                        <div key={index} className="card">
-                            {console.log(item)}
-                            <img src={item.Poster} width="150" alt={item.Title} />
-                            <h4><a href="#" ><Link to={`/DetailsMovie/${item.imdbID}`}>{item.Title}</Link></a></h4>
-                        </div>
+                <InfiniteScroll
+                    dataLength={moviedata.length}
+                    next={fetchMore}
+                    hasMore={totalpages - currentPage !== 0}
+                    loader={<h4>Loading...</h4>}
+                > <div className="cards">
+                        {
 
+                            moviedata.map((item, index) => {
+                                return <div key={index} className="card">
+                                    <img src={item.Poster} width="150" alt={item.Title} />
+                                    <h4><Link to={`/DetailsMovie/${item.imdbID}`}>{item.Title}</Link></h4>
+                                </div>
+
+
+                            })
+                        }
                     </div>
-                })
+                </InfiniteScroll>
             )
 
         } else {
@@ -31,30 +35,9 @@ function DisplayCity({ moviedata, setResult, fetchMore, currentPage, totalpages 
         }
     }
 
-    const changefn = (e) => {
-        setSearch(e.target.value);
-        if (e.target.value) {
-            let filteredData = moviedata.filter(a => a.Title.toLowerCase().includes(search));
-            filteredData.length > 0 ? setResult(filteredData, true) : setResult(filteredData, false);
-        } else {
-            setResult([], true);
-        }
-
-    }
-
-    // fetchMore
-
-
     return (
         <div >
-            <InfiniteScroll
-                dataLength={moviedata.length}
-                next={fetchMore}
-                hasMore={totalpages - currentPage !== 0}
-                loader={<h4>Loading...</h4>}
-            ></InfiniteScroll>
             <center>
-                {/* <h2>Search Movie</h2> */}
                 <table className="titlebar">
                     <tbody>
                         <tr>
@@ -62,15 +45,15 @@ function DisplayCity({ moviedata, setResult, fetchMore, currentPage, totalpages 
                                 Search Movie
                             </td>
                             <td>
-                                <input type="text" placeholder="SearchMovie" aria-label="search" value={search} onChange={changefn} />
+                                <input type="text" placeholder="SearchMovie" aria-label="search" value={search} onChange={updateSearch} />
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </center>
-            <div className="cards">
-                {renderlist(moviedata)}
-            </div>
+
+            {renderlist(moviedata)}
+
         </div>
     )
 }
